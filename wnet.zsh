ef339e9ba6a72fa95d3641d6c7e8d7a775533885
Shawn Webb <shawn.webb@hardenedbsd.org>
2018-04-02 22:51:26 -0400

    Support randomly generating MAC address
    And a few other things.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- wnet.zsh
+++ wnet.zsh
@@ -4,8 +4,14 @@ config="/home/shawn/projects/wnet/conf/networks.conf"
 device="iwm0"
 network=""
 wlan="wlan0"
+extraconfig=""
+sleeptime=5
 
-while getopts "c:d:n:w:" o; do
+scan=""
+randomize=0
+ether=""
+
+while getopts "rc:d:n:s:w:" o; do
 	case "${o}" in
 		c)
 			config="${OPTARG}"
@@ -16,11 +22,21 @@ while getopts "c:d:n:w:" o; do
 		n)
 			network="${OPTARG}"
 			;;
-		wlan)
+		r)
+			randomize=1
+			;;
+		s)
+			sleeptime=${OPTARG}
+			;;
+		w)
 			wlan="${OPTARG}"
 			;;
 	esac
 done
+
+###################
+# Sanity checking #
+###################
 
 if [ -z ${network} ]; then
 	echo "[-] Pass in the network, dummy!" >& 2
@@ -31,6 +47,10 @@ if [ ! -f ${config} ]; then
 	echo "[-] ${config} does not exist!" >& 2
 	exit 1
 fi
+
+#################################
+# Fetch options for the network #
+#################################
 
 ssid=$(uclcmd get -f ${config} --noquotes .${network}.ssid)
 if [ "${ssid}" = "null" ]; then
@@ -44,17 +64,50 @@ if [ "${psk}" = "null" ]; then
 	exit 1
 fi
 
-ifconfig ${wlan} create wlandev ${device}
+scan=$(uclcmd get -f ${config} --noquotes .${network}.scan)
+if [ "${scan}" = "true" ]; then
+	extraconfig="${extraconfig}\nscan_ssid=1"
+fi
+
+#######################################
+# If the interface exists, destroy it #
+#######################################
+
+ifconfig ${wlan} > /dev/null 2>&1
+if [ ${?} -eq 0 ]; then
+	ifconfig ${wlan} destroy
+	res=${?}
+	if [ ${res} -gt 0 ]; then
+		echo "[-] Could not destroy old ${wlan} interface" >&2
+		exit 1
+	fi
+fi
+
+############################
+# Create the new interface #
+############################
+
+ifconfig ${wlan} create wlandev ${device} ${ether}
 res=${?}
 if [ ${res} -gt 0 ]; then
 	echo "[-] Could not create ${wlan} device backed by ${device}" >&2
 	exit 1
 fi
 
+if [ ${randomize} -gt 0 ]; then
+	ifconfig ${wlan} ether random
+	res=${?}
+	if [ ${res} -gt 0 ]; then
+		echo "[-] Could not set random MAC address" >&2
+		exit 1
+	fi
+fi
+
 cat <<EOF | wpa_supplicant -i ${wlan} -B -c /dev/stdin
 network={
 	ssid="${ssid}"
 	psk="${psk}"
+	${extraconfig}
 }
 EOF
 res=${?}
@@ -62,6 +115,20 @@ if [ ${res} -gt 0 ]; then
 	echo "[-] wpa_supplicant failed" >&2
 	exit 1
 fi
+
+####################################
+# Wait for the device to associate #
+####################################
+
+for ((i = 0; i < ${sleeptime}; i++)); do
+	isup=$(ifconfig ${wlan} | grep '^[[:space:]]status:' | awk '{print $2;}')
+	if [ ${isup} = "associated" ]; then
+		break
+	fi
+	echo -n "."
+	sleep 1
+done
+echo
 
 dhclient ${wlan}
 res=${?}
